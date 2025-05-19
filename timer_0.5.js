2949cee2f926c6c89a781bfa575d74b751432e12
Tumzzz21 <aldrinlance19@gmail.com>
2025-05-19 18:53:45 +0800

    Delete timer_0.5.js

--- timer_0.5.js
+++
@@ -1,8 +0,0 @@
-const baseHrs = parseFloat(document.getElementById('interval').value);
-const randHrs = parseFloat(document.getElementById('variance').value);
-
-// Only allow .0 or .5 values
-if ((baseHrs * 10) % 5 !== 0 || (randHrs * 10) % 5 !== 0) {
-  alert('Only .5-hour intervals are allowed (e.g. 11, 11.5, 12, etc.)');
-  return;
-}
